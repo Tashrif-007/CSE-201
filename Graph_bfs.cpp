7aa4942016baa711ce5f56ce50db4ea66be15493
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-05-07 16:14:32 +0600

    Create Graph_bfs.cpp

---
+++ Graph_bfs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+const int MAXN = 1005;
+
+bool g[5][5]={{0,1,1,0,0},
+                {1,0,0,1,0},
+                {1,0,0,1,0},
+                {0,1,1,0,1},
+                {0,0,0,1,0}
+    };
+bool visited[MAXN];
+
+void bfs(int start, int n) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    while(!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+
+        for(int i=1; i<=n; i++) {
+            if(g[curr][i] && !visited[i]) {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+}
+int main(){
+
+    bfs(2,5);
+    return 0;
+}
