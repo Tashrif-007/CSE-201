4bd2e6d7096d411eb6a2472a7493900acf56be4a
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-05-17 15:53:54 +0600

    Create dfs.cpp

---
+++ dfs.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+string color[20];
+int Adj[20][20] = {0};
+int d[20], tim;
+int pre[20], f[20];
+
+void dfs_visit(int u, int numV)
+{
+    color[u] = "GREY";
+    tim++;
+    d[u] = tim;
+
+    for (int v = 0; v < numV; v++)
+    {
+        if (Adj[u][v] && color[v] == "WHITE")
+        {
+            pre[v] = u;
+            dfs_visit(v, numV);
+        }
+    }
+    color[u] = "BLACK";
+    tim++;
+    f[u] = tim;
+}
+
+void dfs(int numV)
+{
+    for (int u = 0; u < numV; u++)
+    {
+        color[u] = "WHITE";
+        pre[u] = -1;
+        f[u] = d[u] = -1;
+    }
+    tim = 0;
+
+    for (int u = 0; u < numV; u++)
+    {
+        if (color[u] == "WHITE")
+            dfs_visit(u, numV);
+    }
+}
+
+int main()
+{
+    int numVertices, numEdges;
+    cout<<"Enter the number of vertices: ";
+    cin >> numVertices;
+
+    cout << "Enter the number of edges: ";
+    cin >> numEdges;
+
+    cout << "Enter the edges (vertex1 vertex2):\n";
+    for (int i = 0; i < numEdges; i++)
+    {
+        int vertex1, vertex2;
+        cin >> vertex1 >> vertex2;
+        Adj[vertex1][vertex2] = 1;
+    }
+
+    dfs(numVertices);
+
+    cout << "Vertex\tDiscovery time\tFinish time\n";
+    for (int u = 0; u < numVertices; u++)
+    {
+        cout << u << "\t" << d[u] << "\t\t" << f[u] << "\n";
+    }
+    return 0;
+}
